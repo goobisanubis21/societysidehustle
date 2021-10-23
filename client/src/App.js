f5e5c92a5340febac9c914968b257dedf3ddce92
Anthony Parrino <anthony.parrino@yahoo.com>
2021-10-23 19:53:21 -0400

    created homepage file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/HomeComp/HomeComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homeComp.css';
+
+function HomeComp() {
+    return (
+        <div>
+            home
+        </div>
+    )
+}
+
+export default HomeComp
