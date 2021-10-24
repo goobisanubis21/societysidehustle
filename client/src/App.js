6abd5bc5e28bd23fe5aecaaeaf70894abccbc18f
Anthony Parrino <anthony.parrino@yahoo.com>
2021-10-24 00:43:54 -0400

    created login auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from './pages/Home';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Signup from "./components/Signup/Signup";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './pages/Home'
+import Login from "./components/Login/Login";
+import PrivateRoutes from "./components/PrivateRoutes/PrivateRoutes";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/signup" component={Signup} />
+                <PrivateRoutes exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
-      </Router>
     </div>
   );
 }
--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './homeComp.css';
 
+import {useAuth} from "../../context/AuthContext"
+
+
 function HomeComp() {
+
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError('')
+
+        try {
+            await logout()
+            history.push("/login")
+        } catch {
+            setError("Failed to log out")
+        }
+    }
+
     return (
         <div>
-            home
+            <div className='home-main'>
+                home page
+            </div>
         </div>
     )
 }
